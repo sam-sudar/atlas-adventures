5a0ea546570764de211f861ac1a483b7bb8461e9
iLLiSiO2 <samsudar200@gmail.com>
2025-01-27 11:23:19 +0530

    First commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+function Homepage() {
+    return (
+        <div>
+            Atlas Adventures Home Page
+        </div>
+    )
+}
+
+export default Homepage
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+function PageNotFound() {
+    return (
+        <div>
+            Page not Found
+        </div>
+    )
+}
+
+export default PageNotFound
