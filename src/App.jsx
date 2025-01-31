5c242ba53f89e127a2382498bb6737d33c5776f6
iLLiSiO2 <samsudar200@gmail.com>
2025-01-31 21:32:00 +0530

    Adding CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Product from "./pages/Product"
-import Homepage from "./pages/Homepage"
-import Pricing from "./pages/Pricing"
-import PageNotFound from "./pages/PageNotFound"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Homepage />} />
+          <Route path="/" element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,27 +1,28 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
 
 function PageNav() {
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <NavLink to="/pricing">Pricing</NavLink>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <NavLink to="/product">Product</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    )
+  return (
+    <div>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <NavLink to="/pricing">Pricing</NavLink>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <NavLink to="/product">Product</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default PageNav
+export default PageNav;
